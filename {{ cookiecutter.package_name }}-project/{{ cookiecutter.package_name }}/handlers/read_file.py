034f80e59f62bf31da1c0a96a07b8def8c4ed2da
Sanhe Hu <husanhe@gmail.com>
2023-01-29 20:55:53 -0500

    c

--- {{ cookiecutter.package_name }}-project/{{ cookiecutter.package_name }}/handlers/read_file.py
+++ {{ cookiecutter.package_name }}-project/{{ cookiecutter.package_name }}/handlers/read_file.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""
+[CN]
+
+该 Script Filter 的功能是展示 file.txt 文件中的内容. 仅仅是和 ``write_file.py`` 模块
+配合使用, 永远验证.
+"""
+
 import sys
 import afwf
 import attr
@@ -9,7 +16,7 @@ from ..paths import dir_project_home
 
 @attr.define
 class Handler(afwf.Handler):
-    def low_level_api(self) -> afwf.ScriptFilter:
+    def main(self) -> afwf.ScriptFilter:
         sf = afwf.ScriptFilter()
         path = dir_project_home / "file.txt"
         if path.exists():
@@ -19,17 +26,16 @@ class Handler(afwf.Handler):
                 subtitle=content,
             )
         else:
-            content = "file.txt does not exist!"
             item = afwf.Item(
-                title=f"Write {content!r} to {path.basename}",
-                arg=f"{sys.executable} main.py 'write_request_handler {content}'",
-            ).set_icon(afwf.IconFileEnum.error)
+                title="file.txt does not exist!",
+            )
+            item.set_icon(afwf.IconFileEnum.error)
 
         sf.items.append(item)
         return sf
 
-    def handler(self, query: str) -> afwf.ScriptFilter:
-        return self.low_level_api()
+    def parse_query(self, query: str):
+        return {}
 
 
 handler = Handler(id="read_file")
